09fb227252b9f9a9f40e2562c9a54921979a0b6b
kambe <50097614+nobuaki0331@users.noreply.github.com>
2021-05-01 20:35:49 +0900

    [add] 入力したフォームに対して赤枠をつける　watchとwatchEffectの動作の違いを確かめる為に使用

--- resources/js/Pages/Components/TextInput.vue
+++ resources/js/Pages/Components/TextInput.vue
@@ -8,12 +8,15 @@
       <input
         :value="modelValue"
         :id="name"
+        :class="{ 'border-red-500' : isActiveClass }"
         class="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         @input="$emit('update:modelValue', $event.target.value)">
   </div>
 </template>
 
 <script>
+import { ref, watch, watchEffect } from "vue";
+
 export default {
   name: 'TextIput',
   props: {
@@ -29,6 +32,19 @@ export default {
       type: [ String, Number ],
       default: ''
     },
+  },
+  setup(props) {
+    const isActiveClass = ref(false)
+    watch(
+      () => props.modelValue,
+      () => {
+        isActiveClass.value = true
+      }
+    )
+
+    return {
+      isActiveClass,
+    }
   },
 }
 </script>

85d7bbff9e104cd2d5b22e5e3718cb90315c795f
nobuaki0331 <z51.kambe.nobuaki@gmail.com>
2021-10-02 15:56:20 +0900

    no message

--- tests/Feature/PasswordConfirmationTest.php
+++ tests/Feature/PasswordConfirmationTest.php
@@ -11,17 +11,6 @@ class PasswordConfirmationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_confirm_password_screen_can_be_rendered()
-    {
-        $user = Features::hasTeamFeatures()
-                        ? User::factory()->withPersonalTeam()->create()
-                        : User::factory()->create();
-
-        $response = $this->actingAs($user)->get('/user/confirm-password');
-
-        // $response->assertStatus(200);
-    }
-
     public function test_password_can_be_confirmed()
     {
         $user = User::factory()->create();

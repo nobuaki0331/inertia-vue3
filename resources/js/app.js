9ea464d06dd62996207615b4b231fca082a36b9c
kambe <50097614+nobuaki0331@users.noreply.github.com>
2021-10-27 20:34:18 +0900

    [add] typescript、pinia状態管理ライブラリ追加（試してみたいので）

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { createPinia } from 'pinia';
 
 const el = document.getElementById('app');
 
@@ -16,6 +17,7 @@ createApp({
 })
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
+    .use(createPinia())
     .mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
